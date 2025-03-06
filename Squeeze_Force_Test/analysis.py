a166137cd29464b2dae989a468d5bb423b8324b8
Didi Zhou <dz34@rice.edu>
2025-03-05 20:06:42 -0600

    updating squeeze testing data

--- Squeeze_Force_Test/analysis.py
+++ Squeeze_Force_Test/analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#for didi: navigate to Fall24 Coursework
-data = np.loadtxt('data.txt')
+
+data = np.loadtxt('/Data/data.txt')
 
 force = data[:, 0:3]   
 torque = data[:, 3:6]  
